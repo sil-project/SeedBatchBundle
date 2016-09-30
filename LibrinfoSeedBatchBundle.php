991aea8cf5142ff2558b8fa3e8876c9c9e078da7
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-09-30 17:47:12 +0200

    Created App Circles

--- LibrinfoSeedBatchBundle.php
+++ LibrinfoSeedBatchBundle.php
@@ -3,7 +3,15 @@
 namespace Librinfo\SeedBatchBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Librinfo\SeedBatchBundle\DependencyInjection\Compiler\AppCirclesPass;
 
 class LibrinfoSeedBatchBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AppCirclesPass());
+    }
 }
