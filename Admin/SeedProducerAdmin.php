663878cee0b3edb799ed92388045ccc969f596c7
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-12-21 11:09:09 +0100

    Use OrganismAdmin instead of OrganismAdminConcrete

--- Admin/SeedProducerAdmin.php
+++ Admin/SeedProducerAdmin.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Librinfo\SeedBatchBundle\Admin;
 
-use Librinfo\SeedBatchBundle\Admin\OrganismAdminConcrete as BaseOrganismAdminConcrete;
+use Librinfo\SeedBatchBundle\Admin\OrganismAdmin as BaseOrganismAdmin;
 
-class SeedProducerAdmin extends BaseOrganismAdminConcrete
+class SeedProducerAdmin extends BaseOrganismAdmin
 {
     protected $baseRouteName = 'admin_librinfo_seedbatch_seedProducer';
     protected $baseRoutePattern = 'librinfo/seedbatch/seed-producer';
@@ -14,7 +22,7 @@ class SeedProducerAdmin extends BaseOrganismAdminConcrete
         $query = parent::createQuery($context);
 
         $query->andWhere('o.seedProducer = true');
-        
+
         return $query;
     }
 }
