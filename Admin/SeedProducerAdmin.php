1c13294b3f5e5a0151fd119d978ac5cad9823345
Romain <romain.sanchez@libre-informatique.fr>
2016-12-21 11:34:49 +0100

    Added direct creation of SeedProducers

--- Admin/SeedProducerAdmin.php
+++ Admin/SeedProducerAdmin.php
@@ -2,13 +2,23 @@
 
 namespace Librinfo\SeedBatchBundle\Admin;
 
+use Doctrine\ORM\EntityManager;
 use Librinfo\SeedBatchBundle\Admin\OrganismAdminConcrete as BaseOrganismAdminConcrete;
 
 class SeedProducerAdmin extends BaseOrganismAdminConcrete
 {
+    /**
+     *
+     * @var EntityManager
+     */
+    private $manager;
+    
     protected $baseRouteName = 'admin_librinfo_seedbatch_seedProducer';
     protected $baseRoutePattern = 'librinfo/seedbatch/seed-producer';
 
+    /**
+     * {@inheritdoc}
+     */
     public function createQuery($context = 'list')
     {
         $query = parent::createQuery($context);
@@ -17,4 +27,27 @@ class SeedProducerAdmin extends BaseOrganismAdminConcrete
         
         return $query;
     }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getNewInstance()
+    {
+        $object = parent::getNewInstance();
+       
+        $object->setSeedProducer(true);
+        $object->addCircle($this->manager->getRepository('LibrinfoCRMBundle:Circle')->find('b907691c-963f-4a7c-9098-5a95335cf21d'));
+        
+        return $object;
+    }
+    
+    /**
+     * 
+     * @param EntityManager $manager
+     */
+    public function setManager(EntityManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
 }
